a52982c05600931626019848e50fc20b14d1bd01
bbkkloulou <166871266+bbkkloulou@users.noreply.github.com>
2024-04-13 01:11:28 +0200

    Add files via upload

---
+++ script.index.js
@@ -0,0 +1,9 @@
+document.getElementById("indexForm").onsubmit = function(event) {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+    alert("Veuillez remplir tous les champs.");
+    event.preventDefault();
+  }	
+};
